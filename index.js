ecada4475b06bc050b8c22c11d87b41db57828e2
Dan Hackworth <danielhackworth515@gmail.com>
2023-10-08 16:50:56 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const findMatching = (drivers, inputDriver) =>{
+    console.log(drivers)
+    return drivers.filter((name)=>
+         name === inputDriver || name === inputDriver.toLowerCase() || name === inputDriver.toUpperCase());
+        
+       
+    
+}
+
+const fuzzyMatch = (drivers, inputDriver2) => {
+    return drivers.filter((name)=>
+        name.slice(0,3).includes(inputDriver2))
+}
+
+const matchName = (drivers, inputDriver3) => {
+    return drivers.filter((driver)=>
+    driver.name === inputDriver3)
+}
+
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
